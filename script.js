5da5e0b0ed01c76f63ee203de31b0ee72ecfd38d
JKimBio <167825551+JKimBio@users.noreply.github.com>
2024-10-22 08:54:07 -1000

    Moolatasticscript.js

---
+++ script.js
@@ -0,0 +1,60 @@
+body {
+  font: 15px arial, sans-serif;
+  background-color: #7e6a66;
+  padding-top: 15px;
+  padding-bottom: 15px;
+}
+
+#bodybox {
+  color: #7e6a66;
+  margin-top: 500px;
+  margin-bottom: 150px;
+  margin-right: 150px;
+  margin-left: 80px;;
+  max-width: 1000px;
+  font: 15px arial, sans-serif;
+  background-color: white;
+  border-style: solid;
+  border-width: 1px;
+  padding-top: 20px;
+  padding-bottom: 25px;
+  padding-right: 25px;
+  padding-left: 25px;
+  box-shadow: 5px 5px 5px grey;
+  border-radius: 15px;
+}
+
+#chatborder {
+  border-style: solid;
+  background-color: #f6f9f6;
+  border-width: 3px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  padding-top: 10px;
+  padding-bottom: 15px;
+  padding-right: 20px;
+  padding-left: 15px;
+  border-radius: 15px;
+}
+
+.chatlog {
+   font: 15px arial, sans-serif;
+}
+
+#chatbox {
+  font: 17px arial, sans-serif;
+  height: 22px;
+  width: 100%;
+  margin:auto;
+}
+
+h1 {
+  margin: auto;
+}
+
+pre {
+  background-color: #f0f0f0;
+  margin-left: 20px;
+}
